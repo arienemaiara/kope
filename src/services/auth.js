95db41b69a350182acedf4f3413119fc83018e1e
Ariene Maiara <ariene.maiara@hotmail.com>
2020-05-01 22:15:14 -0300

    Autenticação estabelecimento

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,23 +2,16 @@ import api from './api';
 
 export function autenticar(userType, user, password) {
 
-	const endPoint = userType === 'cliente' ? '/clientes/login' : '/estabelecimentos/login';
-
-	return api.post(endPoint, {
-		cpf: user,
-		password
-	});
-
-    // return new Promise(resolve => {
-    //     setTimeout(() => {
-    //         resolve({
-    //             user: {
-    //               id: 1,
-    //               cpf: "33212057009",
-    //               nome: "Jao Silva"
-    //             },
-    //             token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidGlwb1VzdWFyaW8iOiJjbGllbnRlIiwiaWF0IjoxNTg4MzQ0OTE4LCJleHAiOjE1ODg5NDk3MTh9.CApEb6gAnT96wViCWIC9114xVk5f59cSobsmOOTnzw0"
-    //           })
-    //     }, 2000);
-    // });
+	if (userType === 'cliente') {
+		return api.post('/clientes/login', {
+			cpf: user,
+			password
+		});
+	}
+	else {
+		return api.post('/estabelecimentos/login', {
+			cpf_cnpj: user,
+			password
+		});
+	}
 }
